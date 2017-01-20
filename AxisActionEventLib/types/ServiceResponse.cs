ef45394f5c20a667384df3a6feb57fecd185e90a
dtrammer <trammerd@gmail.com>
2017-01-20 21:25:55 +0100

    Add files via upload

--- AxisActionEventLib/types/ServiceResponse.cs
+++ AxisActionEventLib/types/ServiceResponse.cs
@@ -1,7 +1,6 @@
 ﻿using ActionEventLib.action;
 using ActionEventLib.events;
 using ActionEventLib.templates;
-using AxisActionEventLib.events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,12 +23,15 @@ namespace ActionEventLib.types
             return new T() { IsSuccess = this.IsSuccess , HttpStatusCode = this.HttpStatusCode , SOAPContent = this.SOAPContent , Content = this.Content };
         }
     }
-
-    public class GetActionTemplatesResponse : ServiceResponse { public List<ActionTemplate> Templates = new List<ActionTemplate>(); }
-    public class GetRecipientTemplatesResponse : ServiceResponse { public List<RecipientTemplate> Templates = new List<RecipientTemplate>(); }
+    /// <summary>
+    /// Adds a List<Actiontemplate> to the response object
+    /// </summary>
+    public class GetActionTemplatesResponse : ServiceResponse { public Dictionary<string, ActionTemplate> Templates = new Dictionary<string, ActionTemplate>(); }
+    public class GetRecipientTemplatesResponse : ServiceResponse { public Dictionary<string,RecipientTemplate> Templates = new Dictionary<string, RecipientTemplate>(); }
     public class GetActionConfigurationsResponse : ServiceResponse { public List<ActionConfiguration> Configurations = new List<ActionConfiguration>(); }
     public class GetActionRulesResponse : ServiceResponse { public List<ActionRule> ActionRules = new List<ActionRule>(); }
     public class GetRecipientConfigurationsResponse : ServiceResponse { public List<RecipientConfiguration> Configurations = new List<RecipientConfiguration>(); }
     public class GetEventInstancesResponse : ServiceResponse { public List<EventTrigger> EventInstances = new List<EventTrigger>(); }
     public class GetScheduledEventsResponse : ServiceResponse { public List<ScheduledEvent> ScheduledEvents = new List<ScheduledEvent>();  }
+
 }
